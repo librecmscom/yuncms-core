4e8972d21cec13e002215b82e5bd784ca462fd29
Peter <xutongle@gmail.com>
2017-11-06 19:54:58 +0800

    update

--- backend/controllers/SettingsController.php
+++ backend/controllers/SettingsController.php
@@ -39,7 +39,7 @@ class SettingsController extends Controller
 //        return [
 //            'toggle' => [
 //                'class' => ToggleAction::className(),
-//                'modelClass' => 'yuncms\system\models\Setting',
+//                'modelClass' => 'yuncms\core\models\Settings',
 //                //'setFlash' => true,
 //            ]
 //        ];
