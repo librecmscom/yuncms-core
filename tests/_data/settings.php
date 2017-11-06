6b365ac6f5bfa7881ebddc7cf698c5e592a11e62
Peter <xutongle@gmail.com>
2017-11-06 19:23:29 +0800

    tests

---
+++ tests/_data/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+return [
+    'url' => [
+        'type' => 'string',
+        'section' => 'system',
+        'key' => 'url',
+        'value' => 'https://www.yuncms.net',
+        'active' => 1,
+        'created' => date('Y-m-d H:i:s'),
+        'modified' => date('Y-m-d H:i:s')
+    ],
+];
