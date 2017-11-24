2caf8f3bfdeeeb215b672132d80ab7ec24265b4c
Peter <xutongle@gmail.com>
2017-11-24 11:23:16 +0800

    utf8mb4

--- migrations/M171104093141Create_session_table.php
+++ migrations/M171104093141Create_session_table.php
@@ -15,7 +15,7 @@ class M171104093141Create_session_table extends Migration
         switch ($this->db->driverName) {
             case 'mysql':
                 // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-                $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+                $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
                 break;
             case 'sqlsrv':
             case 'mssql':
